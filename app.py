4e1e1eac637b92b2b9a89e52674220f2482590b1
Michael Mohamed <michael.jobsearch.emails@gmail.com>
2024-09-13 23:40:54 +0000

    Add registration functionality

--- app.py
+++ app.py
@@ -21,12 +21,30 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_user")
 def get_user():
-    user = mongo.db.user.find()
-    return render_template("user.html", user = user)
+    users = mongo.db.user.find()
+    return render_template("user.html", users = users)
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        #check if username exists in database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
 
 
